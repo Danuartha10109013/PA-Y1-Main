7c788ac85eb33aa1d8af8c897aaaece63944445b
Danuartha <133625622+Danuartha10109013@users.noreply.github.com>
2025-05-15 11:06:20 +0700

    Update index.blade.php

--- resources/views/pages/admin/gaji/index.blade.php
+++ resources/views/pages/admin/gaji/index.blade.php
@@ -1,3 +1,65 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a9f43c665192bc933e1c686e4930bad0ee7c617f98f6e148c9c31bb26a020a42
-size 4369
+@if (auth()->user()->roles == 'admin')
+    @extends('layouts.dashboard-layout')
+    @section('title', $title)
+    @section('content')
+        <div class="content-background" style="background: white">
+            <h3>Potongan Gaji Pinjaman</h3>
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead style="background-color: #EEEEEE;">
+                        <tr>
+                            <th>No</th>
+                            <th>Nama Anggota</th>
+                            <th>Nomor Pinjaman</th>
+                            <th>Jenis Pinjaman</th>
+                            <th>Pokok Pinjaman</th>
+                            <th>Angsuran Pokok</th>
+                            <th>Tenor</th>
+                            <th>Sisa Angsuran</th>
+                            <th>Sisa Bayar (Bulan)</th>
+                            <th>Status</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($pinjamanAktif as $key => $data)
+                            <tr>
+                                <td>{{ $key + 1 }}</td>
+                                <td>{{ $data->user->name }}</td>
+                                <td>{{ $data->nomor_pinjaman }}</td>
+                                <td>{{ ucwords(str_replace('_', ' ', $data->jenis_pinjaman)) }}</td>
+                                <td>Rp {{ number_format($data->amount, 2) }}</td>
+                                <td>Rp {{ number_format($data->nominal_angsuran, 2) }}</td>
+                                <td><span class="badge badge-border-success">{{ $data->jangka_waktu }} Bulan </span>
+                                
+
+                                </td>
+                                <td>Rp {{ number_format($data->sisa_pinjaman, 2) }}</td>
+                                <td><span class="badge badge-border-warning">{{ $data->sisa_jangka_waktu }} Bulan</span>
+                                </td>
+                                <td>
+                                    <span
+                                        class="badge {{ $data->status_pembayaran === 'Aktif' ? 'badge-border-warning' : 'badge-border-success' }}">
+                                        {{ $data->status_pembayaran }}
+                                    </span>
+                                </td>
+                                <td class="action-icons">
+                                    <a href="{{ route('detail.potongan.gaji', $data->uuid) }}">
+                                        <i class="fa fa-solid fa-eye"></i>
+                                    </a>
+                                    @php
+                                        $month = \Carbon\Carbon::now()->format('M-Y');
+                                        // dd($month);
+                                    @endphp
+                                    <a href="{{ route('input.potongan.gaji.create', ['uuid' => $data->uuid, 'month' => $month ]) }}">
+                                        <i class="fa fa-solid fa-edit"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endsection
+@endif
