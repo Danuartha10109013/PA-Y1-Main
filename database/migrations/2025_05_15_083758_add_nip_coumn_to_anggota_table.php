005ee4b1de77b1734cb520ca994bf7abed0bb2b6
Danuartha10109013 <odanuartha@gmail.com>
2025-05-16 00:15:38 +0700

    add column table

---
+++ database/migrations/2025_05_15_083758_add_nip_coumn_to_anggota_table.php
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d329f46d596727b5a580c9d831b92734cc9d006c99ddf31a6f1d0b4a226aacc8
+size 558
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5617dc05481a4521a23f2a3bb85f706f41e4ca1a75bc997203bd5898aabb89d
+oid sha256:76b3bccd4a2285f75ba3c50ba1af44ab31ebc63bf82463bd5d24d4d52a5c7362
 size 9586
