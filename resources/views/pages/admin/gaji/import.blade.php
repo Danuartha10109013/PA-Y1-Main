78ad61b0b0a7f5521fd39e35ff794cad46a1773c
Danuartha <133625622+Danuartha10109013@users.noreply.github.com>
2025-05-15 11:07:50 +0700

    Update import.blade.php

--- resources/views/pages/admin/gaji/import.blade.php
+++ resources/views/pages/admin/gaji/import.blade.php
@@ -1,3 +1,29 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c39ece7a56d0f2077617554d2503ef9c1fa49fd9febe8d3da5bc36460c54fab3
-size 977
+@extends('layouts.dashboard-layout')
+@section('title', $title)
+@section('content')
+    <div class="content-background col-md-6">
+        @if (session('error'))
+            <div class="alert alert-danger">
+                {{ session('error') }}
+            </div>
+        @endif
+
+        <form action="{{ route('potongan.gaji.store', ['uuid' => $pinjamanAktif->uuid, 'month' => $month ]) }}" method="post" enctype="multipart/form-data">
+            @csrf
+
+            <div class="form-group">
+                <label for="bukti_pembayaran">Upload Bukti Pemotongan Gaji</label>
+                <input type="file" id="bukti_pembayaran" name="bukti_pembayaran" class="form-control @error('bukti_pembayaran') is-invalid @enderror" accept="image/*">
+                
+                @error('bukti_pembayaran')
+                    <div class="invalid-feedback d-block">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="text-center">
+                <a href="{{ route('data.potongan.gaji') }}" class="btn btn-secondary">Batalkan</a>
+                <button type="submit" class="btn btn-success">Upload</button>
+            </div>
+        </form>        
+    </div>
+@endsection
