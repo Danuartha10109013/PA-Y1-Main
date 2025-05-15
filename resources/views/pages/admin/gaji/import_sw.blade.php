4d081d1aa2f0d0fa1981ebb27fd9d37c810062d4
Danuartha <133625622+Danuartha10109013@users.noreply.github.com>
2025-05-15 11:06:50 +0700

    Update import_sw.blade.php

--- resources/views/pages/admin/gaji/import_sw.blade.php
+++ resources/views/pages/admin/gaji/import_sw.blade.php
@@ -2,14 +2,26 @@
 @section('title', $title)
 @section('content')
     <div class="content-background col-md-6">
-        <form action="{{ route('potongan.gaji.store.sw', $pinjamanAktif->id) }}" method="post" enctype="multipart/form-data">
+        @if (session('error'))
+            <div class="alert alert-danger">
+                {{ session('error') }}
+            </div>
+        @endif
+
+        <form action="{{ route('potongan.gaji.store', ['uuid' => $pinjamanAktif->uuid, 'month' => $month ]) }}" method="post" enctype="multipart/form-data">
             @csrf
+
             <div class="form-group">
                 <label for="bukti_pembayaran">Upload Bukti Pemotongan Gaji</label>
-                <input type="file" id="bukti_pembayaran" name="bukti_pembayaran" class="form-control">
+                <input type="file" id="bukti_pembayaran" name="bukti_pembayaran" class="form-control @error('bukti_pembayaran') is-invalid @enderror" accept="image/*">
+                
+                @error('bukti_pembayaran')
+                    <div class="invalid-feedback d-block">{{ $message }}</div>
+                @enderror
             </div>
+
             <div class="text-center">
-                <a href="{{ route('data.potongan.gaji.sw') }}" class="btn btn-secondary">Batalkan</a>
+                <a href="{{ route('data.potongan.gaji') }}" class="btn btn-secondary">Batalkan</a>
                 <button type="submit" class="btn btn-success">Upload</button>
             </div>
         </form>        
